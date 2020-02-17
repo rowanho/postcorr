b3f17d36d42ece4a096c27290828cca20b7e186e
rowanho <rowanho45@gmail.com>
2020-02-17 13:32:37 +0000

    More scraping from wikisource

--- python-utils/scraping/scrape_wikisource.py
+++ python-utils/scraping/scrape_wikisource.py
@@ -1,8 +1,12 @@
+import collections
+import os
+import sys
 from bs4 import BeautifulSoup
 import requests
 
+base = 'https://en.wikisource.org'    
+
 def get_validated_links():
-    base = 'https://en.wikisource.org'    
     ext = '/w/index.php?title=Category:Index_Validated'
     links_found = []
     has_next = True
@@ -18,7 +22,6 @@ def get_validated_links():
                 
         # Find the 'next page' link
         next = parser.find('a', title='Category:Index Validated', text='next page')
-        print(next)
         if next == None:
             has_next = False
         else:
@@ -26,7 +29,88 @@ def get_validated_links():
             
     return links_found
     
+def get_reuse_metric(k, text):
+    freqs = collections.defaultdict(int)
+    for i in range(0, len(text) + 1- k):
+        freqs[text[i: i + k]] += 1
+    s = 0
+    i = 0
+    for val in freqs.values():
+        s += val
+        i += 1
+    if i == 0:
+        return 0
+    else:
+        return s / i
+
+def get_page_links(ext):
+    links = []
+    res = requests.get(base + ext)
+    parser = BeautifulSoup(res.text, 'html.parser')
+    potential_page_links = parser.find_all('a', class_='prp-pagequality-4 quality4')
+    for p in potential_page_links:
+        if p.getText().isnumeric():
+            links.append(p['href'])
+    return links
+
+def get_high_scorers(links, n, k):
+    link_scores = []
+    for l in links:
+        page_texts = []
+        page_links = get_page_links(l)
+        for pl in page_links:
+            page_texts.append(get_page_text(pl))
+        text = '\n'.join(page_texts)
+        score = get_reuse_metric(k, text)
+        print(len(text), score)
+        link_scores.append((score, l))
+        
+    link_scores.sort(key=lambda x: x[0], reverse=True)
+    return link_scores[:n]    
+        
+def get_page_text(ext):
+    text = []
+    res = requests.get(base + ext)
+    parser = BeautifulSoup(res.text, 'html.parser')
+    wrapper_div = parser.find('div', class_='mw-parser-output')
+    paragraphs = wrapper_div.find_all('p')
+    for p in paragraphs:
+        text.append(p.getText())
+    return '\n'.join(text)
+    
+    
+def download_page_texts(dir, ext):
+    n = 0
+    page_links = get_page_links(ext)
+    os.mkdir(dir)
+    for l in page_links:
+        text = get_page_text(l)    
+        with open(os.path.join(dir, f'{n}.txt')) as file:
+            file.write(text)
+        n += 1
+
+def download_page_images(dir, ext):
+    n = 0
+    page_links = get_page_links(ext)
+    os.mkdir(dir)
+    for l in page_links:
+        text = get_page_text(l)    
+        with open(os.path.join(dir, f'{n}.txt')) as file:
+            file.write(text)
+        n += 1
+    
+def hs():
+    links = get_validated_links()
+    hs = get_high_scorers(links, 10, 30)
+    print(hs)
+
+def main():
+    ext = sys.argv[1]
+    image_dir = sys.argv[2]
+    text_dir = sys.argv[3]
+    
+    download_page_images(ext, image_dir)
+    download_page_texts(ext, text_dir)
     
 if __name__ == '__main__':
-    links = get_validated_links()
-    print(links)
+    main()
