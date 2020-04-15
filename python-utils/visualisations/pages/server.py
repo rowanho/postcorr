267e08ff332db89a3087011a5b325ead0b537324
rowanho <rowanho45@gmail.com>
2020-04-15 15:26:58 +0100

    Hmtl and css

--- python-utils/visualisations/pages/server.py
+++ python-utils/visualisations/pages/server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 import sys
 import os
-import json
+import json#
+import html
+
 corrected_dir = sys.argv[1]
 
 app = Flask(__name__)
+
+def to_html(text):
+	return html.escape(text).replace("\n", "<br>")
 
 @app.route('/serve_reuse', methods=['post'])
 def serve_reuse():
@@ -29,24 +34,23 @@ def serve_reuse():
 	reuse_map = {}
 	alternating_segments = ""
 	if len(text_segment_list) == 0:
-		alternating_segments = f'<div>{file_text}</div>'
+		alternating_segments = f'<div>{to_html(file_text)}</div>'
 
 	prev = 0
 	print(edits_list)
 	for i, text_segment in enumerate(text_segment_list):
 		start = start_ends_list[i]['start']
 		end = start_ends_list[i]['end'] + 1
-		alternating_segments += f'<div>{file_text[prev:start]}</div>'
+		alternating_segments += f'<span>{to_html(file_text[prev:start])}</span>'
 		encoded = get_encoded(file_text[start:end], edits_list[i])
-		alternating_segments += f'<div class="reused">{encoded}</div>'
+		alternating_segments += f'<span class="reused">{encoded}</span>'
 		prev = end
 		if i == len(text_segment_list) - 1:
-			alternating_segments += f'<div>{file_text[end:]}</div>'
+			alternating_segments += f'<span>{to_html(file_text[end:])}</span>'
 		reuse_map[encoded] = text_segment
 	return jsonify({'segments':alternating_segments, 'reuse_map': reuse_map})
 
 def get_encoded(text_segment, edits):
-	print(edits)
 	if len(edits) == 0:
 		return text_segment
 	text_segment_chars = list(text_segment)
@@ -54,7 +58,10 @@ def get_encoded(text_segment, edits):
 	for i in range(len(text_segment_chars)):
 		if str(i) in edits:
 			typ = edits[str(i)]
-			text_segment_chars[i] = f'<mark class="{typ}"' + text_segment_chars[i] + '</mark>'
+			text_segment_chars[i] = f'<mark class="{typ}">' + to_html(text_segment_chars[i]) + '</mark>'
+			print(text_segment_chars[i])
+		else:
+			text_segment_chars[i] = to_html(text_segment_chars[i])
 	return ''.join(text_segment_chars)
 
 
--- python-utils/visualisations/pages/static/script.js
+++ python-utils/visualisations/pages/static/script.js
@@ -1,7 +1,23 @@
 var searchString = window.location.search
 var params = new URLSearchParams(searchString);
 var pathname = params.get("filename");
-$.post( "/serve_reuse", {"filename": pathname}, function( data ) {
-	console.log(data)
-	$("#main").html(data['segments']);
+var globaldict;
+// Get the modal
+
+$(document).ready(function(){
+	var modal = document.getElementById("reuse-modal");
+	$.post( "/serve_reuse", {"filename": pathname}, function( data ) {
+		console.log(data)
+		$("#main").html(data['segments']);
+		globaldict = data;
+	});
+
+	$(".close").on("click", function() {
+		modal.style.display = "none";
+	});
+
+	$("#main").on("click", ".reused", function() {
+		console.log('clicked on reused');
+		modal.style.display = "block";
+	});
 });
