66cd0e1c4baf54581732bc0f7182e0f98ac050dc
rowanho <rowanho45@gmail.com>
2020-04-27 18:35:22 +0100

    New logging

--- python-utils/visualiser/server.py
+++ python-utils/visualiser/server.py
@@ -23,6 +23,7 @@ new_se_json = open_log_json('vote_start_ends_new.json')
 sub_edit_json = open_log_json('edit_graph_sub.json')
 del_edit_json = open_log_json('edit_graph_del.json')
 ins_edit_json = open_log_json('edit_graph_ins.json')
+vote_json = open_log_json('vote_details.json')
 
 @app.route('/serve_reuse', methods=['post'])
 def serve_reuse():
@@ -37,7 +38,7 @@ def serve_reuse():
 	sub_edits_list = sub_edit_json[filename]
 	del_edits_list = del_edit_json[filename]
 	ins_edits_list = ins_edit_json[filename]
-	merged_edits_list = {**sub_edits_list, **ins_edits_list}
+	vote_details = vote_json[filename]
 	reuse_map = {}
 	alternating_segments = ""
 	if len(text_segment_list) == 0:
@@ -52,7 +53,7 @@ def serve_reuse():
 		end = new_start_ends_list[i]['end'] + 1
 		text_segment[filename + '_unedited'] = get_encoded(original_file_text[old_start:old_end], old_start, del_edits_list)
 		alternating_segments += f'<span>{to_html(file_text[prev:start])}</span>'
-		encoded = get_encoded(file_text[start:end], start, merged_edits_list)
+		encoded = get_encoded_double(file_text[start:end], start, sub_edits_list, ins_edits_list, vote_details)
 		text_segment[filename] = encoded
 		uid = str(uuid4())
 		alternating_segments += f'<span class="reused" uid="{uid}">{encoded}</span>'
@@ -62,7 +63,6 @@ def serve_reuse():
 		escaped_text_segment = {}
 		reuse_map[uid] = get_modal_html(text_segment, filename)
 	return jsonify({'segments':alternating_segments, 'reuse_map': reuse_map})
-
 
 def get_encoded(text_segment, start, edits):
 	if len(edits) == 0:
@@ -77,6 +77,30 @@ def get_encoded(text_segment, start, edits):
 			text_segment_chars[i - start] = to_html(text_segment_chars[i - start])
 	return ''.join(text_segment_chars)
 
+
+def get_encoded_double(text_segment, start, sub_edits, ins_edits, vote_details):
+	if len(sub_edits) + len(ins_edits) == 0:
+		return text_segment
+	text_segment_chars = list(text_segment)
+	pos = 0
+	ins_streak = 0
+	uid = ''
+	for i in range(start, len(text_segment_chars) + start):
+		if str(i) in sub_edits:
+			ins_streak = 0
+			typ = sub_edits[str(i)]
+			hover = get_hover_text(vote_details[str(i)], True)
+			text_segment_chars[i - start] = f'<mark class="{typ} vote">' + to_html(text_segment_chars[i - start]) + hover + '</mark>'
+		elif str(i) in ins_edits:
+			ins_streak += 1
+			hover = get_hover_text(vote_details[str(i - ins_streak)], False)
+			typ = ins_edits[str(i)]
+			text_segment_chars[i - start] = f'<mark class="{typ} vote">' + to_html(text_segment_chars[i - start]) + hover + '</mark>'
+		else:
+			ins_streak = 0
+			text_segment_chars[i - start] = to_html(text_segment_chars[i - start])
+	return ''.join(text_segment_chars)
+
 def get_modal_html(text_segment, leader_key):
 	html_text = []
 	html_text.append('<table style="table-layout:fixed; width:100%; word-break: break-all">')
@@ -87,6 +111,31 @@ def get_modal_html(text_segment, leader_key):
 			build_row(f'Witness ({key})', val, html_text)
 	html_text.append('</table>')
 	return '\n'.join(html_text)
+
+def get_hover_text(jsn, is_sub):
+	if is_sub:
+		dct = jsn['EditDict']
+	else:
+		dct = jsn['InsertDict']
+	print(dct)
+	mx = 0
+	mx_key = ''
+	for key, val in dct.items():
+		if val > mx:
+			mx = val
+			mx_key = key
+	text = []
+	text.append(f'{process(mx_key)}: {mx}')
+	for key, val in dct.items():
+		if key != mx_key:
+				text.append(f'{process(key)}: {val}')
+	t = '\n'.join(text)
+	r =  f'<span class = "hovertext"> {t}</span>'
+	print(r)
+	return r
+
+def process(txt):
+	return to_html(txt).replace('<br>', ' ')
 
 def build_row(key, val, html_text, esc=True):
 	html_text.append('<tr>')
